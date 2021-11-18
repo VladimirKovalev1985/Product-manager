ca45139e42ae182e4a841dfdfa12c7cf2c6573ba
Vladimir Kovalev <boban_envorter@mail.ru>
2021-11-18 19:44:51 +0300

    start

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,24 @@
+package ru.netology.domain;
+
+import ru.netology.domain.Product;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+
+    public Smartphone(){
+        super();
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
